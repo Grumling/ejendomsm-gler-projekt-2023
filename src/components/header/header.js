2f92235a4228617c3ac542f360907160c3ab6514
Mike B. K <mikekristiansen88@gmail.com>
2023-11-10 08:51:08 +0100

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,7 @@ import styles from './header.module.css'
 const HeaderComponent = () => {
   return (
     <div className={styles.headerContainer}>
-      HEADER / NNAVIGATION BAR
+      HEADER / NAVIGATION BAR
     </div>
   )
 }
